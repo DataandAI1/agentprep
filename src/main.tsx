9534d5a061032245698336a56112debba83c093d
dataandaiinnovations <dataandaiinnovations@gmail.com>
2025-10-15 04:39:08 +0000

    Initial updates

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { app } from './firebase/config';
+
+// A simple way to use 'app' to satisfy TypeScript, assuming Firebase initialization happens in config.ts
+console.log('Firebase app initialized:', app);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
